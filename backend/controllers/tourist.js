f45a93666256d6e042fe24b93791104e222adb23
Hanafi Yaming <142217119+jhin3tytwoo@users.noreply.github.com>
2024-03-23 22:04:25 +0700

    add backend

---
+++ backend/controllers/tourist.js
@@ -0,0 +1,1 @@
+const Tourist = require("../models/tourist");
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,18 @@
-const express  = require('express');
+const express = require("express");
 const app = express();
 
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
 });
 
 app.listen(8080, () => {
-    console.log('Server is running on port 11230');
+  console.log("Server is running on port 11230");
 });
