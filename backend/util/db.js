2a3ba1431c60017609354f03af279e7b3083163b
teentsw3421 <tassawas3421@gmail.com>
2024-03-23 21:22:26 +0700

    intitial_commit

--- backend/util/db.js
+++ backend/util/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host : 'srv1278.hstgr.io',
-    user : 'u134026110_64127087',
-    password : 'pE4=s!TBj',
-    database : 'u134026110_64127087'
+    host : 'localhost',
+    user : 'root',
+    password : '',
+    database : 'tourist'
 });
 
 module.exports = pool.promise();
