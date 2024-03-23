02e13169247652322e2cfdeee9d77425766b4500
teentsw3421 <tassawas3421@gmail.com>
2024-03-23 21:25:50 +0700

    commit

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,5 @@ app.use((req, res, next) => {
 });
 
 app.listen(8080, () => {
-<<<<<<< HEAD
     console.log('Server is running on port 11230');
-=======
-    console.log('Server is running on port 8080');
->>>>>>> 1eaad8088c9495477c3a7cfb534da0c191d28611
 });
